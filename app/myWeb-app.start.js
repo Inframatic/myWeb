c4f1b9d89986424a1d996551a6c0d753c9f6e7f4
inframatic <isteffy@viewgov.com>
2015-04-26 09:42:18 -0400

    graphical touch ups in portfolio section

--- app/myWeb-app.start.js
+++ app/myWeb-app.start.js
@@ -3,9 +3,9 @@ angular.module('MyWeb', [
 .controller('MainControl', function($scope){
     $scope.name = 'Ian Steffy';
     $scope.projects = [
-      { "id": 0, "title": "Skreens", "url": "img/skreensTV-logo.png", "description": "HTML5, CSS3, Responsive, Design, jQuery"},
-      { "id": 1, "title": "Doge Dots", "url": "some/img/url.png", "description": "Design, XCode, Responsive, ASO, Animation, App Publishing "},
-      { "id": 2, "title": "ViewPoint Cloud", "url": "some/img/url.png", "description": "HTML5, CSS3, JQuery, Javascript, Ember, Handlebars"}
+      { "id": 0, "title": "Skreens", "url": "img/skreensTV-logo.png", "description": "HTML5, CSS3, Responsive, Design, jQuery", "backgroundColor" : "#000"},
+      { "id": 2, "title": "ViewPoint Cloud", "url": "img/viewpoint-logo.png", "description": "HTML5, CSS3, JQuery, Javascript, Ember, Handlebars", "backgroundColor" : "#00a7e1"},
+      { "id": 1, "title": "Doge Dots", "url": "img/dogedots-logo.png", "description": "Design, XCode, Responsive, ASO, Animation, App Publishing ", "backgroundColor" : "#eee"}
     ]
   })
 ;
