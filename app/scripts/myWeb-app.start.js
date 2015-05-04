4b0dc0471e4e17e796ade63ecc627011325f8787
inframatic <isteffy@viewgov.com>
2015-05-04 11:02:23 -0400

    added yo. hello yo

--- app/scripts/myWeb-app.start.js
+++ app/scripts/myWeb-app.start.js
@@ -6,5 +6,26 @@ angular.module('MyWeb', [])
       { "id": 2, "title": "ViewPoint Cloud", "url": "img/viewpoint-logo.png", "description": "HTML5, CSS3, JQuery, Javascript, Ember, Handlebars", "backgroundColor" : "#00a7e1"},
       { "id": 1, "title": "Doge Dots", "url": "img/dogedots-logo.png", "description": "Design, XCode, Responsive, ASO, Animation, App Publishing ", "backgroundColor" : "#eee"}
     ]
-  })
-;
+})
+  // .config(function($routeProvider, $locationProvider) {
+  //   $routeProvider
+  //    .when('/Book/:bookId', {
+  //     templateUrl: 'book.html',
+  //     controller: 'BookController',
+  //     resolve: {
+  //       // I will cause a 1 second delay
+  //       delay: function($q, $timeout) {
+  //         var delay = $q.defer();
+  //         $timeout(delay.resolve, 1000);
+  //         return delay.promise;
+  //       }
+  //     }
+  //   })
+  //   .when('/Book/:bookId/ch/:chapterId', {
+  //     templateUrl: 'chapter.html',
+  //     controller: 'ChapterController'
+  //   });
+
+  //   // configure html5 to get links working on jsfiddle
+  //   $locationProvider.html5Mode(true);
+  // });
